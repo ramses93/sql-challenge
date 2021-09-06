d5f3c45b7f8bd514f1f930fff7b98d57cc2a1531
ramses93 <ramirezelida11@yahoo.com>
2021-09-06 15:05:51 -0400

    Edited Employee command
    Edited the command to load data into Employees. It now parses correctly the birth_date and hire_date

--- fill_from_csv_commands.sql
+++ fill_from_csv_commands.sql
@@ -2,7 +2,10 @@ LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/employees.
 INTO TABLE Employees
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+IGNORE 1 ROWS
+(emp_no, emp_title_id, @birth_date, first_name, last_name, sex, @hire_date)
+SET birth_date = STR_TO_DATE(@birth_date, '%m/%d/%Y'),
+hire_date = STR_TO_DATE(@hire_date, '%m/%d/%Y');
 
 LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/salaries.csv'
 INTO TABLE Salaries
