6afae9548758337552b45635f7879b4f164eb1eb
ramses93 <ramirezelida11@yahoo.com>
2021-09-06 14:14:56 -0400

    Updated Commands
    Updated commants. Fixed some errors.

--- QuickDBD-export.sql
+++ QuickDBD-export.sql
@@ -1,87 +1,61 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
--- Modify this code to update the DB schema diagram.
--- To reset the sample schema, replace everything with
--- two dots ('..' - without quotes).
-
-CREATE TABLE "Employees" (
-    "emp_no" VARCHAR  NOT NULL ,
-    "emp_tile_id" VARCHAR  NOT NULL ,
-    "birth_date" DATE  NOT NULL ,
-    "first_name" VARCHAR  NOT NULL ,
-    "last_name" VARCHAR  NOT NULL ,
-    "sex" CHAR(1)  NOT NULL ,
-    "hire_date" DATE  NOT NULL ,
-    CONSTRAINT "pk_Employees" PRIMARY KEY (
-        "emp_no"
+﻿CREATE TABLE `Employees` (
+    `emp_no` VARCHAR(10),
+    `emp_tile_id` VARCHAR(10),
+    `birth_date` DATE,
+    `first_name` VARCHAR(20),
+    `last_name` VARCHAR(20),
+    `sex` CHAR(1),
+    `hire_date` DATE,
+    CONSTRAINT `pk_Employees` PRIMARY KEY (
+        `emp_no`
     )
-)
+);
 
-GO
-
-CREATE TABLE "Titles" (
-    "title_id" VARCHAR  NOT NULL ,
-    "title" VARCHAR  NOT NULL ,
-    CONSTRAINT "pk_Titles" PRIMARY KEY (
-        "title_id"
+CREATE TABLE `Titles` (
+    `title_id` VARCHAR(10),
+    `title` VARCHAR(30),
+    CONSTRAINT `pk_Titles` PRIMARY KEY (
+        `title_id`
     )
-)
+);
 
-GO
+CREATE TABLE `Salaries` (
+    `emp_no` VARCHAR(10),
+    `salary` INT NOT NULL DEFAULT 0 
+);
 
-CREATE TABLE "Salaries" (
-    "emp_no" VARCHAR  NOT NULL ,
-    "salary" INT  NOT NULL 
-)
+CREATE TABLE `DeptManager` (
+    `dept_no` VARCHAR(10),
+    `emp_no` VARCHAR(10) 
+);
 
-GO
+CREATE TABLE `DeptEmp` (
+    `emp_no` VARCHAR(10),
+    `dept_no` VARCHAR(10) 
+);
 
-CREATE TABLE "DeptManager" (
-    "dept_no" VARCHAR  NOT NULL ,
-    "emp_no" VARCHAR  NOT NULL 
-)
-
-GO
-
-CREATE TABLE "DeptEmp" (
-    "emp_no" VARCHAR  NOT NULL ,
-    "dept_no" VARCHAR  NOT NULL 
-)
-
-GO
-
-CREATE TABLE "Departments" (
-    "dept_no" VARCHAR  NOT NULL ,
-    "dept_name" VARCHAR  NOT NULL ,
-    CONSTRAINT "pk_Departments" PRIMARY KEY (
-        "dept_no"
+CREATE TABLE `Departments` (
+    `dept_no` VARCHAR(10),
+    `dept_name` VARCHAR(30),
+    CONSTRAINT `pk_Departments` PRIMARY KEY (
+        `dept_no`
     )
-)
+);
 
-GO
+ALTER TABLE `Employees` ADD CONSTRAINT `fk_Employees_emp_tile_id` FOREIGN KEY(`emp_tile_id`)
+REFERENCES `Titles` (`title_id`);
 
-ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_tile_id" FOREIGN KEY("emp_tile_id")
-REFERENCES "Titles" ("title_id")
-GO
+ALTER TABLE `Salaries` ADD CONSTRAINT `fk_Salaries_emp_no` FOREIGN KEY(`emp_no`)
+REFERENCES `Employees` (`emp_no`);
 
-ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no")
-GO
+ALTER TABLE `DeptManager` ADD CONSTRAINT `fk_DeptManager_dept_no` FOREIGN KEY(`dept_no`)
+REFERENCES `Departments` (`dept_no`);
 
-ALTER TABLE "DeptManager" ADD CONSTRAINT "fk_DeptManager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no")
-GO
+ALTER TABLE `DeptManager` ADD CONSTRAINT `fk_DeptManager_emp_no` FOREIGN KEY(`emp_no`)
+REFERENCES `Employees` (`emp_no`);
 
-ALTER TABLE "DeptManager" ADD CONSTRAINT "fk_DeptManager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no")
-GO
+ALTER TABLE `DeptEmp` ADD CONSTRAINT `fk_DeptEmp_emp_no` FOREIGN KEY(`emp_no`)
+REFERENCES `Employees` (`emp_no`);
 
-ALTER TABLE "DeptEmp" ADD CONSTRAINT "fk_DeptEmp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no")
-GO
-
-ALTER TABLE "DeptEmp" ADD CONSTRAINT "fk_DeptEmp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no")
-GO
-
+ALTER TABLE `DeptEmp` ADD CONSTRAINT `fk_DeptEmp_dept_no` FOREIGN KEY(`dept_no`)
+REFERENCES `Departments` (`dept_no`);
