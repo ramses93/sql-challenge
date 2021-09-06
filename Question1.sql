e24ee6366fe73799968264c2b203900a64b3d9f0
ramses93 <ramirezelida11@yahoo.com>
2021-09-06 18:00:48 -0400

    Question 1 command
    List the following details of each employee: employee number, last name, first name, sex, and salary.

---
+++ Question1.sql
@@ -0,0 +1,1 @@
+SELECT A.emp_no AS 'Employee Number', A.last_name AS 'Last Name', A.first_name AS 'First Name', A.sex AS 'Sex', B.salary AS 'Salary' FROM Employees AS A INNER JOIN Salaries AS B ON A.emp_no = B.emp_no; 
