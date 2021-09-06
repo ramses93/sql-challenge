2ec344a6c8465ae687c84f900b0869a64b1f9691
ramses93 <ramirezelida11@yahoo.com>
2021-09-06 18:42:26 -0400

    Updated Question 2 command
    Changed for better readability

--- Question2.sql
+++ Question2.sql
@@ -1,1 +1,7 @@
-SELECT emp_no AS 'Employee Number', first_name AS 'First Name', last_name AS 'Last Name', sex AS 'Sex' FROM Employees AS A WHERE year(hire_date) = 1986;
+SELECT
+	emp_no AS 'Employee Number',
+    first_name AS 'First Name',
+    last_name AS 'Last Name',
+    sex AS 'Sex'
+FROM Employees AS A
+WHERE year(hire_date) = 1986;
